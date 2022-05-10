91ed701dfd21262991ae77bb0581000fb01e3694
Rodolpho Caetano <Rodolpho Caetano@DESKTOP-AMAHFPJ.mshome.net>
2022-05-10 20:03:43 -0300

    gross salary and tax update

--- salary_exercise/src/Main.java
+++ salary_exercise/src/Main.java
@@ -1,8 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
 public class Main {
 
 	public static void main(String[] args) {
-
-
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		Employee employee = new Employee();
+		
+		System.out.println("Name: ");
+		employee.name = sc.nextLine();
+		
+		System.out.println("Gross salary: ");
+		employee.grossSalary = sc.nextDouble();
+		
+		System.out.println("Tax: ");
+		employee.tax = sc.nextDouble();
+		
+		System.out.println(employee);
+		
+		
+		sc.close();
 	}
 
 }
